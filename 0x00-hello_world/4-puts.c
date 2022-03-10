4568b4bf558a90c1e5e21b7805985af9b4656d3d
fellydyll <yeboahfelix022@gmail.com>
2022-03-10 09:16:08 -0800

    print string using put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - This code print string using
+ * puts
+ */
+int main(void)
+{
+	char ch[] = "Programming is like bulding a multilngual puzzle";
+	puts(ch);
+	return (0);
+}
