b9736af5133b2e04096c08d4e6d9a37c444a1528
fellydyll <yeboahfelix022@gmail.com>
2022-03-11 10:20:30 -0800

    print last number and itsvsign

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print last digit of a random
+ * number and assign its sign
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int n;
+	int ls;
+
+	srand(time(0));
+	n = rand() - RAND_MAX;
+	ls = n % 10;
+	if (ls > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, ls);
+	else if (ls == 0)
+		printf("Last digit of %d is %d and is 0\n", n, ls);
+
+	else
+		printf("Last digit of %d is %d and is less than 5\n", n, ls);
+	return (0);
+
+}
