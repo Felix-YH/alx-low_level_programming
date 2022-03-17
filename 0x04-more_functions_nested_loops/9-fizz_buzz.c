52a8d9981a8df4c51d121b49aae6e07f35f6ac0f
felly dyll <yeboahfelix022@gmail.com>
2022-03-17 23:08:39 +0000

    add advanced tasks

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,34 +6,27 @@
  * Return: 0 Always
  */
 
-int main(void)
+iint main(void)
 {
-	int i, j, k;
+	int i;
 
-	for (i = 1; i <= 100; i++)
+	i = 1;
+	while (i <= 100)
 	{
-		j = i % 3;
-		k = i % 5;
-
-		if (j == 0 && k == 0)
-		{
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
-		}
-		else if (k == 0)
-		{
-			printf("Buzz");
-		}
-		else if (j == 0)
-		{
+		else if (i % 3 == 0)
 			printf("Fizz");
-		}
+		else if (i % 5 == 0)
+			printf("Buzz");
 		else
-		{
-			printf("%d ", i);
-		}
+			printf("%d", i);
+
 		if (i != 100)
 			printf(" ");
+		i++;
 	}
 	printf("\n");
+
 	return (0);
 }
