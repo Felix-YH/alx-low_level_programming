dd0bd3329907330e4e94ec1efb7cbfe9e5435347
fellydyll <yeboahfelix022@gmail.com>
2022-03-28 08:05:22 -0700

    update task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,10 +9,10 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, s1, s2; 
+	int i, s1, s2;
+
 	s1 = 0;
 	s2 = 0;
-
 	for (i = 0; i < size; i++)
 	{
 		s1 += a[i];
@@ -25,5 +25,5 @@ void print_diagsums(int *a, int size)
 		a -= size;
 	}
 	printf("%d, %d\n", s1, s2);
-    
+
 }
