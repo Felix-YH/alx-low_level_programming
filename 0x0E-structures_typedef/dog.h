457ceb2ecab803cb0206c813d8ca71b45d7a24e2
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-04-11 22:30:12 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,9 +16,8 @@ struct dog
 };
 
 /**
- * dog_t -change to new type
+ * dog_t - Typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
