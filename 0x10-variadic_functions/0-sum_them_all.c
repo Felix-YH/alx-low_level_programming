5eb4d0342da8d35dce2e2dd04d3a87ced56ccb6d
fellydyll <yeboahfelix022@gmail.com>
2022-04-14 11:50:47 -0700

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,6 +17,8 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(ap, n);
 
 	sum = 0;
+	if (n == 0)
+		return (0);
 
 	for (i = 0; i < n; i++)
 		sum = sum + va_arg(ap, int);
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 
 /**
@@ -18,9 +19,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(ap, n);
 
+	if (separator == NULL)
+		return;
+
 	for (i = 0; i < n; i++)
 	{
-		printf ("%d", va_arg(ap, int));
+		printf("%d", va_arg(ap, int));
 		if (i < n - 1)
 		{
 			printf("%c", *separator);
