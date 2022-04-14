268324012f5e4f8b414dadc24aeef1833ea6d65b
fellydyll <yeboahfelix022@gmail.com>
2022-04-14 12:03:49 -0700

    Update 0-sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,8 +17,6 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(ap, n);
 
 	sum = 0;
-	if (n == 0)
-		return (0);
 
 	for (i = 0; i < n; i++)
 		sum = sum + va_arg(ap, int);
@@ -26,5 +24,4 @@ int sum_them_all(const unsigned int n, ...)
 	va_end(ap);
 
 	return (sum);
-
 }
