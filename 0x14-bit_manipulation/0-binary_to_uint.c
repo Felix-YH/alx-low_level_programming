52b855aeef5e77bc986f513aff13a354dc3b2f3f
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-04-28 15:58:19 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * binary_to_uint - convert a binary number to unit
  * @b: pointer that stores the number
@@ -11,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int cnum = 0, mul = 1;
 	int i = 0;
 
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
 
 	for (i = 0; b[i];)
