cf5d991bd1c54e284773c7bb8569cca9a72a1914
fellydyll <yeboahfelix022@gmail.com>
2022-04-28 07:44:47 -0700

    Create task 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * binary_to_uint - convert a binary number to unit
+ * @b: pointer that stores the number
+ *
+ * Return: converted number or NULL when b is NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int cnum = 0, mul = 1;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i];)
+		i++;
+
+	for (i -= 1; i >= 0; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		cnum += (b[i] - '0') * mul;
+		mul *= 2;
+	}
+	return (cnum);
+}
